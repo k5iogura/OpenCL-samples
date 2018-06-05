2e8548cc71f362eab7d2c0669b0c0f10adde8e13
Kenji Ogura <kenji.ogura@jcom.zaq.ne.jp>
2018-06-05 21:43:18 +0900

    revise

--- gemm_accel/gemm.c
+++ gemm_accel/gemm.c
@@ -32,35 +32,69 @@ void gemm_nn_accel(int M, int N, int K,
         float *C, int ldc)
 {
     int i,j,k;
+    size_t g_work_size;
+    size_t l_work_size;
     cl_int ret=0;
     if(!FPGA_init){
         openCLCreate(&context, &queue, &device);
         openCLCreate_Stage2(context,device,"gemm.cl","gemm_nn",&kernel);
     }
-    cl_mem Acl = clCreateBuffer(context,  CL_MEM_READ_ONLY, M*K*sizeof(float), NULL, &ret);
-    cl_mem Bcl = clCreateBuffer(context,  CL_MEM_READ_ONLY, K*N*sizeof(float), NULL, &ret);
-    cl_mem Ccl = clCreateBuffer(context,  CL_MEM_READ_WRITE, M*N*sizeof(float), NULL, &ret);
-    ret|=clEnqueueWriteBuffer(queue, Acl, CL_TRUE, 0, M*K*sizeof(float), (void*)A, 0, NULL, NULL);
-    ret|=clEnqueueWriteBuffer(queue, Bcl, CL_TRUE, 0, K*N*sizeof(float), (void*)B, 0, NULL, NULL);
-    ret|=clEnqueueWriteBuffer(queue, Ccl, CL_TRUE, 0, M*N*sizeof(float), (void*)C, 0, NULL, NULL);
-    clFinish(queue);
+    cl_mem icl,kcl,ldccl,ldbcl,A_PARTcl,Bcl,Ccl;
     for(i = 0; i < M; ++i){
         for(k = 0; k < K; ++k){
             float A_PART = A[i*lda+k];
-            for(j = 0; j < N; ++j){
+//            for(j = 0; j < N; ++j){
 //                C[i*ldc+j] += A_PART*B[k*ldb+j];
-                ret|=clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&Ccl);
-                ret|=clSetKernelArg(kernel, 1, sizeof(cl_mem), (void*)&Bcl);
-                ret|=clSetKernelArg(kernel, 2, sizeof(cl_mem), (void*)&Acl);
-                ret|=clEnqueueTask(queue,kernel,0,NULL,NULL);
+                icl      = clCreateBuffer(context,  CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
+                kcl      = clCreateBuffer(context,  CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
+                ldccl    = clCreateBuffer(context,  CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
+                ldbcl    = clCreateBuffer(context,  CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
+                A_PARTcl = clCreateBuffer(context,  CL_MEM_READ_ONLY, sizeof(float), NULL, &ret);
+                Bcl      = clCreateBuffer(context,  CL_MEM_READ_ONLY, K*N*sizeof(float), NULL, &ret);
+                Ccl      = clCreateBuffer(context,  CL_MEM_READ_WRITE, M*N*sizeof(float), NULL, &ret);
+                ret|=clEnqueueWriteBuffer(queue, icl, CL_TRUE, 0, sizeof(int), (void*)&i, 0, NULL, NULL);
+                checkErr(ret,"clEnqueueWriteBuffer-0");
+                ret|=clEnqueueWriteBuffer(queue, kcl, CL_TRUE, 0, sizeof(int), (void*)&k, 0, NULL, NULL);
+                checkErr(ret,"clEnqueueWriteBuffer-1");
+                ret|=clEnqueueWriteBuffer(queue, ldccl, CL_TRUE, 0, sizeof(int), (void*)&ldc, 0, NULL, NULL);
+                checkErr(ret,"clEnqueueWriteBuffer-2");
+                ret|=clEnqueueWriteBuffer(queue, ldbcl, CL_TRUE, 0, sizeof(int), (void*)&ldb, 0, NULL, NULL);
+                checkErr(ret,"clEnqueueWriteBuffer-3");
+                ret|=clEnqueueWriteBuffer(queue, Ccl, CL_TRUE, 0, M*N*sizeof(float), (void*)C, 0, NULL, NULL);
+                checkErr(ret,"clEnqueueWriteBuffer-4");
+                ret|=clEnqueueWriteBuffer(queue, Bcl, CL_TRUE, 0, K*N*sizeof(float), (void*)B, 0, NULL, NULL);
+                checkErr(ret,"clEnqueueWriteBuffer-5");
+                ret|=clEnqueueWriteBuffer(queue, A_PARTcl, CL_TRUE, 0, sizeof(float), (void*)&A_PART, 0, NULL, NULL);
+                checkErr(ret,"clEnqueueWriteBuffer-4");
+                ret|=clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&icl);
+                checkErr(ret,"SetKernelArg-0");
+                ret|=clSetKernelArg(kernel, 1, sizeof(cl_mem), &kcl);
+                checkErr(ret,"SetKernelArg-1");
+                ret|=clSetKernelArg(kernel, 2, sizeof(cl_mem), &ldccl);
+                checkErr(ret,"SetKernelArg-2");
+                ret|=clSetKernelArg(kernel, 3, sizeof(cl_mem), &ldbcl);
+                checkErr(ret,"SetKernelArg-3");
+                ret|=clSetKernelArg(kernel, 4, sizeof(cl_mem), (void*)&Ccl);
+                checkErr(ret,"SetKernelArg-4");
+                ret|=clSetKernelArg(kernel, 5, sizeof(cl_mem), (void*)&Bcl);
+                checkErr(ret,"SetKernelArg-5");
+                ret|=clSetKernelArg(kernel, 6, sizeof(cl_mem), (void*)&A_PARTcl);
+                checkErr(ret,"SetKernelArg-6");
+                //ret|=clEnqueueTask(queue,kernel,0,NULL,NULL);
+                g_work_size = l_work_size = N;
+                ret|=clEnqueueNDRangeKernel(queue,kernel,1,0,&g_work_size,&l_work_size,0,NULL,NULL);
+                checkErr(ret,"clEnqueueNDRangeKernel-0");
                 if(ret==CL_SUCCESS){
                     clFinish(queue);
                 }else
                     printf("clSetKernelArg or any error\n");
-            }
+//            }
         }
     }
+    ret = clEnqueueReadBuffer(queue, Ccl, CL_TRUE, 0, M*N*sizeof(float), (void*)C, 0, NULL, NULL);
+    checkErr(ret,"clEnqueueReadBuffer-0");
     for(i=0;i<8;i++)printf("B[%d]=%5.2f\t",i,B[i]);
+    for(i=0;i<8;i++)printf("C[%d]=%5.2f\t",i,C[i]);
     printf("\n");
     //clFinish(queue);
 }
--- gemm_accel/gemm.cl
+++ gemm_accel/gemm.cl
@@ -1,10 +1,21 @@
 __kernel void gemm_nn(
+    __global int *p_i,
+    __global int *p_k,
+    __global int *p_ldc,
+    __global int *p_ldb,
     __global float *C,
     __global const float *B,
-    __global const float *A_PART
+    __global const float *A
 ){
-    printf("IN- %f %f %f\n",C[8],A_PART[8],B[8]);
-    (*C) +=(*A_PART)*(*B);
+    int j = get_global_id(0);
+    int i = *p_i;
+    int k = *p_k;
+    int ldc = *p_ldc;
+    int ldb = *p_ldb;
+    float A_PART=*A;
+    C[i*ldc+j] +=A_PART * B[k*ldb+j];
+    printf("C- %d-%d-%d %f \n",i,k,j, C[i*ldc+j]);
+    printf("B- %d-%d-%d %f \n",i,k,j, B[k*ldb+j]);
 }
 
 
