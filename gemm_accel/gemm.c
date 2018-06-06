ee2b5c4c4433b3a07b9553a40102c45928b6eed2
k5iogura <kenji.ogura@jcom.zaq.ne.jp>
2018-06-06 13:09:25 +0900

    revise

--- gemm_accel/gemm.c
+++ gemm_accel/gemm.c
@@ -26,6 +26,13 @@ static cl_command_queue queue=NULL;
 static cl_kernel kernel=NULL;
 static cl_device_id device=NULL;
 
+void init_fpga(char*kernel_file, char*kernel_name){
+    if(!FPGA_init){
+        openCLCreate(&context, &queue, &device);
+        openCLCreate_Stage2(context,device,kernel_file,kernel_name,&kernel);
+        FPGA_init=1;
+    }
+}
 void gemm_nn_accel(int M, int N, int K,
         float *A, int lda, 
         float *B, int ldb,
@@ -35,57 +42,35 @@ void gemm_nn_accel(int M, int N, int K,
     size_t g_work_size;
     size_t l_work_size;
     cl_int ret=0;
-    if(!FPGA_init){
-        openCLCreate(&context, &queue, &device);
-        openCLCreate_Stage2(context,device,"gemm.cl","gemm_nn",&kernel);
-    }
-    cl_mem icl,kcl,ldccl,ldbcl,A_PARTcl,Bcl,Ccl;
+    cl_mem Bcl,Ccl;
+    Bcl      = clCreateBuffer(context,  CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, K*N*sizeof(float), B, &ret);
+    Ccl      = clCreateBuffer(context,  CL_MEM_READ_WRITE| CL_MEM_USE_HOST_PTR, M*N*sizeof(float), C, &ret);
+    ret|=clSetKernelArg(kernel, 3, sizeof(cl_int), &ldc);        checkErr(ret,"SetKernelArg-3");
+    ret|=clSetKernelArg(kernel, 4, sizeof(cl_int), &ldb);        checkErr(ret,"SetKernelArg-4");
+    ret|=clSetKernelArg(kernel, 5, sizeof(cl_mem), (void*)&Ccl); checkErr(ret,"SetKernelArg-5");
+    ret|=clSetKernelArg(kernel, 6, sizeof(cl_mem), (void*)&Bcl); checkErr(ret,"SetKernelArg-6");
     for(i = 0; i < M; ++i){
         for(k = 0; k < K; ++k){
             float A_PART = A[i*lda+k];
-//            for(j = 0; j < N; ++j){
-//                C[i*ldc+j] += A_PART*B[k*ldb+j];
-                icl      = clCreateBuffer(context,  CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
-                kcl      = clCreateBuffer(context,  CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
-                ldccl    = clCreateBuffer(context,  CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
-                ldbcl    = clCreateBuffer(context,  CL_MEM_READ_ONLY, sizeof(int), NULL, &ret);
-                A_PARTcl = clCreateBuffer(context,  CL_MEM_READ_ONLY, sizeof(float), NULL, &ret);
-                Bcl      = clCreateBuffer(context,  CL_MEM_READ_ONLY, K*N*sizeof(float), NULL, &ret);
-                Ccl      = clCreateBuffer(context,  CL_MEM_READ_WRITE, M*N*sizeof(float), NULL, &ret);
-                ret|=clEnqueueWriteBuffer(queue, icl, CL_TRUE, 0, sizeof(int), (void*)&i, 0, NULL, NULL);
-                checkErr(ret,"clEnqueueWriteBuffer-0");
-                ret|=clEnqueueWriteBuffer(queue, kcl, CL_TRUE, 0, sizeof(int), (void*)&k, 0, NULL, NULL);
-                checkErr(ret,"clEnqueueWriteBuffer-1");
-                ret|=clEnqueueWriteBuffer(queue, ldccl, CL_TRUE, 0, sizeof(int), (void*)&ldc, 0, NULL, NULL);
-                checkErr(ret,"clEnqueueWriteBuffer-2");
-                ret|=clEnqueueWriteBuffer(queue, ldbcl, CL_TRUE, 0, sizeof(int), (void*)&ldb, 0, NULL, NULL);
-                checkErr(ret,"clEnqueueWriteBuffer-3");
-                ret|=clEnqueueWriteBuffer(queue, Ccl, CL_TRUE, 0, M*N*sizeof(float), (void*)C, 0, NULL, NULL);
-                checkErr(ret,"clEnqueueWriteBuffer-4");
-                ret|=clEnqueueWriteBuffer(queue, Bcl, CL_TRUE, 0, K*N*sizeof(float), (void*)B, 0, NULL, NULL);
-                checkErr(ret,"clEnqueueWriteBuffer-5");
-                ret|=clEnqueueWriteBuffer(queue, A_PARTcl, CL_TRUE, 0, sizeof(float), (void*)&A_PART, 0, NULL, NULL);
-                checkErr(ret,"clEnqueueWriteBuffer-4");
-                ret|=clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&icl); checkErr(ret,"SetKernelArg-0");
-                ret|=clSetKernelArg(kernel, 1, sizeof(cl_mem), &kcl);        checkErr(ret,"SetKernelArg-1");
-                ret|=clSetKernelArg(kernel, 2, sizeof(cl_mem), &ldccl);      checkErr(ret,"SetKernelArg-2");
-                ret|=clSetKernelArg(kernel, 3, sizeof(cl_mem), &ldbcl);      checkErr(ret,"SetKernelArg-3");
-                ret|=clSetKernelArg(kernel, 4, sizeof(cl_mem), (void*)&Ccl); checkErr(ret,"SetKernelArg-4");
-                ret|=clSetKernelArg(kernel, 5, sizeof(cl_mem), (void*)&Bcl); checkErr(ret,"SetKernelArg-5");
-                ret|=clSetKernelArg(kernel, 6, sizeof(cl_mem), (void*)&A_PARTcl); checkErr(ret,"SetKernelArg-6");
-                //ret|=clEnqueueTask(queue,kernel,0,NULL,NULL);
-                g_work_size = l_work_size = N;
-                ret|=clEnqueueNDRangeKernel(queue,kernel,1,0,&g_work_size,&l_work_size,0,NULL,NULL);
-                checkErr(ret,"clEnqueueNDRangeKernel-0");
-                if(ret==CL_SUCCESS){
-                    clFinish(queue);
-                }else
-                    printf("clSetKernelArg or any error\n");
-                ret = clEnqueueReadBuffer(queue, Ccl, CL_TRUE, 0, M*N*sizeof(float), (void*)C, 0, NULL, NULL);
-                checkErr(ret,"clEnqueueReadBuffer-0");
-//            }
+            for(j = 0; j < N; ++j){
+                ret|=clSetKernelArg(kernel, 0, sizeof(cl_int), &i);        checkErr(ret,"SetKernelArg-0");
+                ret|=clSetKernelArg(kernel, 1, sizeof(cl_int), &k);        checkErr(ret,"SetKernelArg-1");
+                ret|=clSetKernelArg(kernel, 2, sizeof(cl_int), &j);        checkErr(ret,"SetKernelArg-2");
+                ret|=clSetKernelArg(kernel, 7, sizeof(cl_float), &A_PART); checkErr(ret,"SetKernelArg-7");
+                if(1){
+                    ret|=clEnqueueTask(queue,kernel,0,NULL,NULL);
+                    checkErr(ret,"clEnqueueNDRangeKernel-0");
+                }else{
+                    g_work_size = 1;
+                    ret|=clEnqueueNDRangeKernel(queue,kernel,1,0,&g_work_size,NULL,0,NULL,NULL);
+                    checkErr(ret,"clEnqueueTask-0");
+                }
+                clFinish(queue);
+            }
         }
     }
+    ret = clEnqueueReadBuffer(queue, Ccl, CL_TRUE, 0, M*N*sizeof(float), (void*)C, 0, NULL, NULL);
+    checkErr(ret,"clEnqueueReadBuffer-0");
     for(i=0;i<8;i++)printf("B[%d]=%5.2f\t",i,B[i]);
     for(i=0;i<8;i++)printf("C[%d]=%5.2f\t",i,C[i]);
     printf("\n");
--- gemm_accel/work.c
+++ gemm_accel/work.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
-//#include "CLutil.h"
 #include "gemm.h"
 #include <sys/time.h>
 #include <CL/cl.h>
 
-//#define M (1024)
-//#define N (1024)
-//#define K (1024)
-static int M = 8;
-static int N = 8;
-static int K = 8;
+static int M = 32;
+static int N = 16;
+static int K = 16;
 void makeD(
         float *A,
-        float *B,
-        float *C)
+        float *B)
 {
     int m,n,k;
     for(m=0;m<M*K;m++) A[m]=(float)m;
     for(n=0;n<K*N;n++) B[n]=(float)n;
-    //for(k=0;k<M*N;k++) A[k]=(float)k;
 }
 
-time_t gtime(){
+time_t utime(){
     struct timeval  tv;
     gettimeofday(&tv,NULL);
     return(1000000*tv.tv_sec+tv.tv_usec);
 }
 
 void main(){
+    int i;
     cl_context context;
     cl_command_queue queue;
     cl_device_id device;
@@ -37,32 +32,20 @@ void main(){
     float *A=(float*)calloc(M*K,sizeof(float));
     float *B=(float*)calloc(K*N,sizeof(float));
     float *C=(float*)calloc(M*N,sizeof(float));
-    makeD(A,B,C);
-    time_t s = gtime();
-    gemm_nn_accel(M,N,K,A,K,B,N,C,M);
+    makeD(A,B);
+    time_t s = utime();
+    if(1){
+        init_fpga("gemm.cl","gemm_nn");
+        s = utime();
+        gemm_nn_accel(M,N,K,A,K,B,N,C,M);
+    }else
+        gemm_nn(M,N,K,A,K,B,N,C,M);
+    printf("%fmsec\n",(utime()-s)/1000.);
     for(int i=0;i<8;i++){
         printf("%5.2f\t",A[i]);
     }
     printf("\n");
-    printf("%ldusec\n",gtime()-s);
-/*
-    printf("global space=(%ld,%ld)\n",global_work_size[0],global_work_size[1]);
-    openCLCreate(&context, &queue, &device);
-    openCLCreate_Stage2(context,device,"kernel.cl","global_id",&kernel);
-
-    clEnqueueNDRangeKernel(
-    queue,
-    kernel,
-    2,
-    NULL,
-    global_work_size,
-    NULL,
-    //local_work_size,
-    0,
-    NULL,
-    NULL
-    );
-
-    clFinish(queue);
-*/
+    for(i=0;i<8;i++)printf("B[%d]=%5.2f\t",i,B[i]);
+    for(i=0;i<8;i++)printf("C[%d]=%5.2f\t",i,C[i]);
+    printf("\n");
 }
